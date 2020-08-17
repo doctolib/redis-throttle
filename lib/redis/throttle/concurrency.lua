8c6e12c7cb7bab92da5ee91bbd048d51d39552c3
Alexey Zapparov <alexey@zapparov.com>
2020-08-17 06:23:13 +0200

    Refactor threshold

--- lib/redis/throttle/concurrency.lua
+++ lib/redis/throttle/concurrency.lua
@@ -3,11 +3,11 @@ local bucket, token, limit, ttl, now =
 
 redis.call("ZREMRANGEBYSCORE", bucket, "-inf", "(" .. now)
 
-if limit <= redis.call("ZCARD", bucket) and not redis.call("ZSCORE", bucket, token) then
-  return 1
+if redis.call("ZCARD", bucket) < limit or redis.call("ZSCORE", bucket, token) then
+  redis.call("ZADD", bucket, now + ttl, token)
+  redis.call("EXPIRE", bucket, ttl)
+
+  return 0
 end
 
-redis.call("ZADD", bucket, now + ttl, token)
-redis.call("EXPIRE", bucket, ttl)
-
-return 0
+return 1
