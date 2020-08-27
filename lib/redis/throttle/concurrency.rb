dfb5d0423c8acc4e9029f32d1274f4b3d278f8e0
Alexey Zapparov <alexey@zapparov.com>
2020-08-27 05:38:44 +0200

    Grand Refactoring
    - Concurrency and Threshold are data valeus now
    - Threshold strategies are executed before concurrency
    - Enrich Throttle API

--- lib/redis/throttle/concurrency.rb
+++ lib/redis/throttle/concurrency.rb
@@ -1,45 +1,87 @@
 # frozen_string_literal: true
 
-require_relative "./script"
-
 class Redis
   class Throttle
     class Concurrency
-      attr_reader :key
+      # @!attribute [r] bucket
+      #   @return [String] Throttling group name
+      attr_reader :bucket
 
-      # @param bucket [#to_s]
-      # @param limit [#to_i]
-      # @param ttl [#to_i]
+      # @!attribute [r] limit
+      #   @return [Integer] Max allowed concurrent units
+      attr_reader :limit
+
+      # @!attribute [r] ttl
+      #   @return [Integer] Time (in seconds) to hold the lock before
+      #     releasing it (in case it wasn't released already)
+      attr_reader :ttl
+
+      # @param bucket [#to_s] Throttling group name
+      # @param limit [#to_i] Max allowed concurrent units
+      # @param ttl [#to_i] Time (in seconds) to hold the lock before
+      #   releasing it (in case it wasn't released already)
       def initialize(bucket, limit:, ttl:)
-        @limit = limit.to_i
-        @ttl   = ttl.to_i
-        @key   = "throttle:#{bucket}:c:#{@limit}:#{@ttl}"
+        @bucket = -bucket.to_s
+        @limit  = limit.to_i
+        @ttl    = ttl.to_i
       end
 
-      # @param redis [Redis, Redis::Namespace]
-      # @param token [#to_s]
-      # @return [Boolean]
-      def acquire(redis, token:)
-        Script
-          .eval(redis, [key], lua_payload << token.to_s << Time.now.to_i)
-          .zero?
+      # @api private
+      def key
+        "throttle:#{@bucket}:c:#{@limit}:#{@ttl}"
       end
 
-      # @param redis [Redis, Redis::Namespace]
-      # @param token [#to_s]
-      # @return [void]
-      def release(redis, token:)
-        redis.zrem(key, token.to_s)
-      end
+      alias to_s key
 
-      # @param redis [Redis, Redis::Namespace]
-      # @return [void]
-      def reset(redis)
-        redis.del(key)
-      end
-
-      def lua_payload
+      # @api private
+      def payload
         ["concurrency", @limit, @ttl]
+      end
+
+      # @api private
+      #
+      # Returns `true` if `other` is a {Concurrency} instance with the same
+      # {#bucket}, {#limit}, and {#ttl}.
+      #
+      # @see https://docs.ruby-lang.org/en/master/Object.html#method-i-eql-3F
+      # @param other [Object]
+      # @return [Boolean]
+      def ==(other)
+        equal?(other) || (other.is_a?(self.class) && key == other.key)
+      end
+
+      alias eql? ==
+
+      # @api private
+      #
+      # Compare `self` with `other` strategy:
+      #
+      # - Returns `nil` if `other` is neither {Concurrency} nor {Threshold}
+      # - Returns `1` if `other` is a {Threshold}
+      # - Returns `1` if `other` is a {Concurrency} with lower {#limit}
+      # - Returns `0` if `other` is a {Concurrency} with the same {#limit}
+      # - Returns `-1` if `other` is a {Concurrency} with bigger {#limit}
+      #
+      # @return [-1, 0, 1, nil]
+      def <=>(other)
+        complexity <=> other.complexity if other.respond_to? :complexity
+      end
+
+      # @api private
+      #
+      # Generates an Integer hash value for this object.
+      #
+      # @see https://docs.ruby-lang.org/en/master/Object.html#method-i-hash
+      # @return [Integer]
+      def hash
+        key.hash
+      end
+
+      # @api private
+      #
+      # @return [Array(Integer, Integer)] Strategy complexity pseudo-score
+      def complexity
+        [1, @limit]
       end
     end
   end
--- spec/lib/redis/throttle/concurrency_spec.rb
+++ spec/lib/redis/throttle/concurrency_spec.rb
@@ -1,56 +1,123 @@
 # frozen_string_literal: true
 
 require "redis/throttle/concurrency"
+require "redis/throttle/threshold"
 
 RSpec.describe Redis::Throttle::Concurrency do
-  subject(:concurrency) { described_class.new(:styx, :limit => 2, :ttl => 10) }
+  subject(:concurrency) { described_class.new(:acab, :limit => 13, :ttl => 12) }
 
-  describe "#acquire" do
-    before { concurrency.acquire(Redis.current, :token => :hades) }
+  it { is_expected.to respond_to :bucket }
+  it { is_expected.to respond_to :limit }
+  it { is_expected.to respond_to :ttl }
 
-    it "returns true when bucket allows new tokens" do
-      expect(concurrency.acquire(Redis.current, :token => :charon)).to be true
+  describe "#==" do
+    subject { concurrency == other }
+
+    let(:other)  { described_class.new(bucket, :limit => limit, :ttl => ttl) }
+    let(:bucket) { concurrency.bucket }
+    let(:limit)  { concurrency.limit }
+    let(:ttl)    { concurrency.ttl }
+
+    it { is_expected.to be true }
+
+    context "when bucket differs" do
+      let(:bucket) { "#{concurrency.bucket}x" }
+
+      it { is_expected.to be false }
     end
 
-    context "when concurrency bucket is full" do
-      before { concurrency.acquire(Redis.current, :token => :charon) }
+    context "when limit differs" do
+      let(:limit) { concurrency.limit + 1 }
 
-      it "returns true for the token still holding the lock" do
-        expect(concurrency.acquire(Redis.current, :token => :charon)).to be true
-      end
-
-      it "returns false" do
-        expect(concurrency.acquire(Redis.current, :token => :heracles)).to be false
-      end
-    end
-  end
-
-  describe "#release" do
-    it "does nothing when token was not previously acquired" do
-      concurrency.acquire(Redis.current, :token => :hades)
-      concurrency.acquire(Redis.current, :token => :charon)
-      concurrency.release(Redis.current, :token => :heracles)
-
-      expect(concurrency.acquire(Redis.current, :token => :heracles)).to be false
+      it { is_expected.to be false }
     end
 
-    it "empties bucket by one" do
-      concurrency.acquire(Redis.current, :token => :hades)
-      concurrency.acquire(Redis.current, :token => :charon)
-      concurrency.release(Redis.current, :token => :charon)
+    context "when ttl differs" do
+      let(:ttl) { concurrency.ttl + 1 }
 
-      expect(concurrency.acquire(Redis.current, :token => :heracles)).to be true
+      it { is_expected.to be false }
     end
   end
 
-  describe "#reset" do
-    it "releases all locks" do
-      concurrency.acquire(Redis.current, :token => :hades)
-      concurrency.acquire(Redis.current, :token => :charon)
+  describe "#eql?" do
+    it "is an alias of #==" do
+      expect(concurrency.method(:eql?).original_name).to eq :==
+    end
+  end
 
-      concurrency.reset(Redis.current)
+  describe "#<=>" do
+    subject { concurrency <=> other }
 
-      expect(concurrency.acquire(Redis.current, :token => :heracles)).to be true
+    let(:other)  { described_class.new(bucket, :limit => limit, :ttl => ttl) }
+    let(:bucket) { concurrency.bucket }
+    let(:limit)  { concurrency.limit }
+    let(:ttl)    { concurrency.ttl }
+
+    it { is_expected.to eq 0 }
+
+    context "when bucket differs" do
+      let(:bucket) { "#{concurrency.bucket}x" }
+
+      it { is_expected.to eq 0 }
+    end
+
+    context "when other limit is bigger" do
+      let(:limit) { concurrency.limit + 1 }
+
+      it { is_expected.to eq(-1) }
+    end
+
+    context "when other limit is smaller" do
+      let(:limit) { concurrency.limit - 1 }
+
+      it { is_expected.to eq 1 }
+    end
+
+    context "when ttl differs" do
+      let(:ttl) { concurrency.ttl + 1 }
+
+      it { is_expected.to eq 0 }
+    end
+
+    context "when other is an instance of Threshold" do
+      let(:other) { Redis::Throttle::Threshold.new(bucket, :limit => limit, :period => 123) }
+
+      it { is_expected.to eq 1 }
+    end
+
+    context "when other is neither Concurrency nor Threshold" do
+      let(:other) { double }
+
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe "#hash" do
+    subject { concurrency.hash }
+
+    let(:other)  { described_class.new(bucket, :limit => limit, :ttl => ttl) }
+    let(:bucket) { concurrency.bucket }
+    let(:limit)  { concurrency.limit }
+    let(:ttl)    { concurrency.ttl }
+
+    it { is_expected.to eq other.hash }
+
+    context "when bucket differs" do
+      let(:bucket) { "#{concurrency.bucket}x" }
+
+      it { is_expected.not_to eq other.hash }
+    end
+
+    context "when limit differs" do
+      let(:limit) { concurrency.limit + 1 }
+
+      it { is_expected.not_to eq other.hash }
+    end
+
+    context "when ttl differs" do
+      let(:ttl) { concurrency.ttl + 1 }
+
+      it { is_expected.not_to eq other.hash }
     end
   end
 end
