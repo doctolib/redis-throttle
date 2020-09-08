165ee4b4de9a133cd81b0916dd913c6ab69f25a6
Alexey Zapparov <alexey@zapparov.com>
2020-09-08 02:21:14 +0200

    [rspec] truncate frozen time to seconds-precision
    Hoping to make frozen-time dependent specs more reliable.

--- spec/support/timecop.rb
+++ spec/support/timecop.rb
@@ -5,5 +5,7 @@ require "timecop"
 Timecop.safe_mode = true
 
 RSpec.configure do |config|
-  config.around(:each, :frozen_time) { |example| Timecop.freeze(Time.now.utc, &example) }
+  config.around(:each, :frozen_time) do |example|
+    Timecop.freeze(Time.at(Time.now.to_i), &example)
+  end
 end
